89d02897aafe4adfcc4c05923116517716ca63af
KESHUKUMAR1909 <keshukumar1909@gmail.com>
2026-07-07 17:30:46 +0530

    feat: integrate dynamic CMS page builder & update iframe resizer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingB from "./pages/LandingB";
+import DynamicPageRenderer from "./components/DynamicPageRenderer";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingB />} />
+          <Route path="/p/:slug" element={<DynamicPageRenderer portal="landing2" />} />
           <Route path="*" element={<LandingB />} />
         </Routes>
       </div>
